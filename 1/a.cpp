bdd7b8909c154abf144ffb5d03bb67c82ff02c5c
julianvanmeggelen <julianvanmeggelen@gmail.com>
2024-03-25 21:18:15 +0100

    a cpp

--- 1/a.cpp
+++ 1/a.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib> 
 using namespace std;
 
 int main(){
     string line;
-    ifstream MyReadFile("1.txt");
-    while (getline (MyReadFile, line)) {
-        cout << line;
+    ifstream MyReadFile("input.txt");
+    int res = 0;
+    while (getline(MyReadFile, line)) {
+        vector<char> allNumeric;
+        for(int i =0; i < line.length(); i++){
+            if(isdigit(line[i]) != 0){
+                allNumeric.push_back(line[i]);
+            }
+        }
+        char first = allNumeric[0];
+        char last = allNumeric.back();
+        string conc = string(1,first) + last;
+        int sum = stoi(conc);
+        res += sum;
     }
+    cout << res;
+    return 0;
 }
