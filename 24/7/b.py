e8434dd233f19d1c7b83978390f24f49e4a28770
julianvanmeggelen <julianvanmeggelen@gmail.com>
2024-12-13 21:47:54 +0100

    d7

--- 24/7/b.py
+++ 24/7/b.py
@@ -10,11 +10,9 @@ def check_operators(num: int, curr: float, nums:int, i: int=0):
         or check_operators(num, int(str(curr)+str(nums[0])), nums[1:], i+1)
     )
 
-   
 
 total: int = 0
 for line in open('input.txt'):
-    #print("----"*10)
     left = int(line.strip().split(':')[0])
     nums = [int(_.strip()) for _ in line.split(':')[1].split()]
     if check_operators(left, nums[0], nums[1:]):
